01ea10d53c9c9a99db86bf850e69d0734b71d56d
zishanshaikh111@gmail.com <zishanshaikh111@gmail.com>
2021-05-07 12:47:59 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var dur = document.getElementById("years").value;
+    var interest = (principal * dur * rate) /100;
+    var amount = parseInt(principal)+parseInt(interest);
+    var year = new Date().getFullYear()+parseInt(dur);
+    if (principal <= 0)
+    {
+        alert("Enter a positive number");
+    }
+    else
+    {
+    document.getElementById("pri_val").innerHTML=principal;
+    document.getElementById("r_val").innerHTML=rate;
+    document.getElementById("d_val").innerHTML=dur;
+    document.getElementById("int_val").innerHTML=interest;
+    document.getElementById("y_val").innerHTML=year;
+
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    }
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}        
